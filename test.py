aca7646fdbcca8bdf3f8c1c4ae9b2046b8be51c8
ucrRichard <rduon008@ucr.edu>
2020-06-17 07:53:20 -0700

    updates to test.py and prepared environment

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 import yfinance as yf
+import time
 
-msft = yf.Ticker("MSFT")
-print(msft)
-msft.info
+time1 = time.time()
+stock = yf.Ticker("UONEK")
+time2 = time.time()
+print("Elapsed time for Ticker", time2 - time1)
+
+#stock.info
+time3 = time.time()
+print("Elapsed time for Info", time3 - time2)
